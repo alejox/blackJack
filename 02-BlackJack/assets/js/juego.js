3980ededee7a9b6f0aec6760710c43252cf29342
alejox <alejoxdrums@gmail.com>
2023-04-07 15:57:31 -0500

    update

--- 02-BlackJack/assets/js/juego.js
+++ 02-BlackJack/assets/js/juego.js
@@ -1,6 +1,8 @@
 let deck = [];
 const tipos = ['C', 'H', 'D', 'S'];
-const especiales = ['A', 'J', 'Q', 'K'];
+const especiales = [ 'A', 'J', 'Q', 'K' ];
+
+//Esta función crea una nueva baraja
 
 const crearDeck = () => {
   for (let i = 2; i <= 10; i++) {
@@ -14,6 +16,17 @@ const crearDeck = () => {
     }
   }
   console.log(deck);
+  
+  deck = _.shuffle(deck);
+  console.log(deck);
+  return deck
 };
 
 crearDeck();
+
+
+//Esta función me permite una carta
+
+const pedirCarta = () => {
+  return "2C"
+}
